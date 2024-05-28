2fec08b0b6dfd3a89e609365d452feb67aec2caf
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-28 15:53:13 +0000

    Added Files

---
+++ 00.VPC/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  type    = string
+  default = "expense"
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    Project     = "expense",
+    Environment = "dev",
+    Terraform   = "true"
+  }
+}
+
+variable "public_subnet_cidrs" {
+  default = ["100.0.1.0/24", "100.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["100.0.11.0/24", "100.0.12.0/24"]
+}
+
+variable "database_subnet_cidrs" {
+  default = ["100.0.21.0/24", "100.0.22.0/24"]
+}
+
+variable "enable_peering" {
+  default = true
+}
