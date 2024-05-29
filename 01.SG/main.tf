bf8a5dc01829f29d9701e9b326b4047653827dae
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 04:08:19 +0000

    Added SG

--- 01.SG/main.tf
+++ 01.SG/main.tf
@@ -30,3 +30,25 @@ module "sg-made-easy-fe" {
   ingress        = var.ingress
   common_tags    = var.common_tags
 }
+
+module "sg-made-easy-bastion" {
+  source         = "git::https://github.com/kalidindi-naveen/Learn-TF-Modules//10.Terraform-AWS-SG?ref=main"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_name        = "bastion"
+  vpc_id         = data.aws_ssm_parameter.this.value
+  sg_description = "SG For bastion"
+  ingress        = var.ingress
+  common_tags    = var.common_tags
+}
+
+module "sg-made-easy-ansible" {
+  source         = "git::https://github.com/kalidindi-naveen/Learn-TF-Modules//10.Terraform-AWS-SG?ref=main"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_name        = "ansible"
+  vpc_id         = data.aws_ssm_parameter.this.value
+  sg_description = "SG For ansible"
+  ingress        = var.ingress
+  common_tags    = var.common_tags
+}
--- 01.SG/sg-rules.tf
+++ 01.SG/sg-rules.tf
@@ -1,3 +1,4 @@
+### DB
 resource "aws_security_group_rule" "db-be" {
   type                     = "ingress"
   from_port                = 3306
@@ -7,6 +8,16 @@ resource "aws_security_group_rule" "db-be" {
   security_group_id        = module.sg-made-easy-db.sg_id
 }
 
+resource "aws_security_group_rule" "db-bastion" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-be.sg_id
+  security_group_id        = module.sg-made-easy-bastion.sg_id
+}
+
+### BE
 resource "aws_security_group_rule" "be-fe" {
   type                     = "ingress"
   from_port                = 8080
@@ -16,11 +27,68 @@ resource "aws_security_group_rule" "be-fe" {
   security_group_id        = module.sg-made-easy-be.sg_id
 }
 
-resource "aws_security_group_rule" "fe" {
+resource "aws_security_group_rule" "be-bastion" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-bastion.sg_id
+  security_group_id        = module.sg-made-easy-be.sg_id
+}
+
+resource "aws_security_group_rule" "be-ansible" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-ansible.sg_id
+  security_group_id        = module.sg-made-easy-be.sg_id
+}
+
+### FE
+resource "aws_security_group_rule" "fe-public" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.sg-made-easy-fe.sg_id
+}
+
+resource "aws_security_group_rule" "fe-bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = module.sg-made-easy-bastion.sg_id
+  security_group_id = module.sg-made-easy-fe.sg_id
+}
+
+resource "aws_security_group_rule" "fe-ansible" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = module.sg-made-easy-ansible.sg_id
+  security_group_id = module.sg-made-easy-fe.sg_id
+}
+
+### Bastion
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.sg-made-easy-bastion.sg_id
+}
+
+### Ansible
+resource "aws_security_group_rule" "ansible_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.sg-made-easy-ansible.sg_id
 }
