b18f22334c872fbc51a2656708e42ee7139b0a1a
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 15:18:09 +0000

    Added Ansible

--- 05.Ansible/main.tf
+++ 05.Ansible/main.tf
@@ -1,26 +1,26 @@
 data "aws_ssm_parameter" "this" {
-  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+  name = "/${var.project_name}/${var.environment}/ansible_sg_id"
 }
 
 data "aws_ssm_parameter" "pub_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/pub_subnet_ids"
 }
 
-module "bastion-made-easy" {
+module "ansible-made-easy" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "${var.project_name}-${var.environment}-bastion"
+  name = "${var.project_name}-${var.environment}-ansible"
   ami  = data.aws_ami.ami_id.id
 
   instance_type               = "t2.micro"
   monitoring                  = true
-  user_data                   = file("bastion.sh")
+  user_data                   = file("ansible.sh")
   associate_public_ip_address = true
   vpc_security_group_ids      = [data.aws_ssm_parameter.this.value]
   subnet_id                   = element(split(",", data.aws_ssm_parameter.pub_subnet_ids.value), 0)
 
   tags = {
-    Name        = "${var.project_name}-${var.environment}-bastion",
+    Name        = "${var.project_name}-${var.environment}-ansible",
     Project     = "expense",
     Environment = "dev",
     Terraform   = "true"
