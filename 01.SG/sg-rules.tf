d062eaa25351693e631c965135bc8113a904837f
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 03:19:59 +0000

    Added SG

--- 01.SG/sg-rules.tf
+++ 01.SG/sg-rules.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group_rule" "db-be" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  source            = module.sg-made-easy-be.sg_id
-  security_group_id = module.sg-made-easy-db.sg_id
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-be.sg_id
+  security_group_id        = module.sg-made-easy-db.sg_id
 }
