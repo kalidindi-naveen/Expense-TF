0bfccb66a462fd96778936ed303ea93cbe5f4a06
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 02:54:53 +0000

    Added SG

--- 01.SG/parameters.tf
+++ 01.SG/parameters.tf
@@ -5,17 +5,17 @@ data "aws_ssm_parameter" "this" {
 resource "aws_ssm_parameter" "db-sg" {
   name  = "/${var.project_name}/${var.environment}/db_sg_id"
   type  = "String"
-  value = module.sg-made-easy-db.id
+  value = module.sg-made-easy-db.sg_id
 }
 
 resource "aws_ssm_parameter" "be-sg" {
   name  = "/${var.project_name}/${var.environment}/be_sg_id"
   type  = "String"
-  value = module.sg-made-easy-be.id
+  value = module.sg-made-easy-be.sg_id
 }
 
 resource "aws_ssm_parameter" "fe-sg" {
   name  = "/${var.project_name}/${var.environment}/fe_sg_id"
   type  = "String"
-  value = module.sg-made-easy-fe.id
+  value = module.sg-made-easy-fe.sg_id
 }
