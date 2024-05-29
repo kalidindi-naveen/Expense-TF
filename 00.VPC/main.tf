dcbf7c948f76ab6edfcae18b1e07dcf8c18f74a0
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 04:51:08 +0000

    Added Outputs

--- 00.VPC/main.tf
+++ 00.VPC/main.tf
@@ -4,19 +4,19 @@ resource "aws_ssm_parameter" "this" {
   value = module.vpc-made-easy.vpc_id
 }
 
-resource "aws_ssm_parameter" "this" {
+resource "aws_ssm_parameter" "pub_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/pub_subnet_ids"
   type  = "StringList"
   value = module.vpc-made-easy.pub_subnet_ids
 }
 
-resource "aws_ssm_parameter" "this" {
+resource "aws_ssm_parameter" "pri_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/pri_subnet_ids"
   type  = "StringList"
   value = module.vpc-made-easy.pri_subnet_ids
 }
 
-resource "aws_ssm_parameter" "this" {
+resource "aws_ssm_parameter" "db_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/db_subnet_ids"
   type  = "StringList"
   value = module.vpc-made-easy.pub_subnet_ids
