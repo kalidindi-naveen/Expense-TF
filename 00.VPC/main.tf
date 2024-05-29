c246535a8eb0f623b122cf80ea2ef244bc83efc8
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 04:50:29 +0000

    Added Outputs

--- 00.VPC/main.tf
+++ 00.VPC/main.tf
@@ -4,6 +4,24 @@ resource "aws_ssm_parameter" "this" {
   value = module.vpc-made-easy.vpc_id
 }
 
+resource "aws_ssm_parameter" "this" {
+  name  = "/${var.project_name}/${var.environment}/pub_subnet_ids"
+  type  = "StringList"
+  value = module.vpc-made-easy.pub_subnet_ids
+}
+
+resource "aws_ssm_parameter" "this" {
+  name  = "/${var.project_name}/${var.environment}/pri_subnet_ids"
+  type  = "StringList"
+  value = module.vpc-made-easy.pri_subnet_ids
+}
+
+resource "aws_ssm_parameter" "this" {
+  name  = "/${var.project_name}/${var.environment}/db_subnet_ids"
+  type  = "StringList"
+  value = module.vpc-made-easy.pub_subnet_ids
+}
+
 module "vpc-made-easy" {
   # source                = "../08.Terraform-AWS-VPC-IGW-Subnets-Peering"
   source                = "git::https://github.com/kalidindi-naveen/Learn-TF-Modules.git//08.Terraform-AWS-VPC-IGW-Subnets-Peering?ref=main"
