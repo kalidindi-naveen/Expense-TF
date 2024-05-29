d5c46846f12d913a41e87bc125eaf87bdd400446
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 03:23:34 +0000

    Added SG

--- 01.SG/sg-rules.tf
+++ 01.SG/sg-rules.tf
@@ -6,3 +6,21 @@ resource "aws_security_group_rule" "db-be" {
   source_security_group_id = module.sg-made-easy-be.sg_id
   security_group_id        = module.sg-made-easy-db.sg_id
 }
+
+resource "aws_security_group_rule" "be-fe" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-fe.sg_id
+  security_group_id        = module.sg-made-easy-be.sg_id
+}
+
+resource "aws_security_group_rule" "fe" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  ipv4_cidr_blocks  = ["0.0.0.0/0"]
+  security_group_id = module.sg-made-easy-fe.sg_id
+}
