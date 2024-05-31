3fcf06a0083fa270b41baf2c6080d1023997e485
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-31 06:22:34 +0000

    Added Ansible

--- 05.Ansible/ansible.sh
+++ 05.Ansible/ansible.sh
@@ -4,5 +4,5 @@ dnf install ansible -y
 cd /tmp
 git clone https://github.com/kalidindi-naveen/Expense-Ansible-Roles.git
 cd Expense-Ansible-Roles
-ansible-playbook main.yml -e SERVER=be -e login_password=ExpenseApp1
+ansible-playbook main.yml -e SERVER=be -e PASSWORD=ExpenseApp1
 ansible-playbook main.yml -e SERVER=fe
